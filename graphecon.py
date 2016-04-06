5e6c66898335d3b7371c993bd9c55753828c7ab3
Nehal Kamat <nehal.kamat@colorado.edu>
2016-04-05 23:30:31 -0600

    Calculating correct best purchase values for paper example

--- graphecon.py
+++ graphecon.py
@@ -23,136 +23,86 @@ class Utility(object):
         x[i]=utilities[i]
         return x
 
-                
 
-class LinearUtility(Utility):
-
+class LinearUtility:
     def __init__(self, weights):
-        self.weights = weights
+        self.weights = np.array(map(float, weights))
 
-    
     def bestPurchase(self, price_vec, budget):
-        #Calculate best purchase for linear, given a price vector and the budget
-        #return the index of the maximum
-        i = np.argmax(self.weights/price_vec)
+        i = np.argmax(self.weights/np.array(map(float,price_vec)))
         x = np.zeros(price_vec.shape)
         x[i] = budget/price_vec[i]
-        
+
         return x
-       
-  
-    '''
-    def bestPurchase(self, price_vec, budget):
-        #other method, returns max utility
-        for n in range(0, len(price_vec)):
-            price_vec[n]=budget/price_vec[n]
-        x = np.zeros(price_vec.shape)
-        index = np.argmax(price_vec)
-        x[index]=price_vec[index]*self.weights[index]
-       
-        return x
-    '''
-   
 
-class NonLinearUtility(Utility):
 
-    def __init__(self, weights):
-        self.weights = weights
-
-    #def bestPurchase(self, price_vec, budget):
-        
-        #return [eval(self.weights, {'math': math, '__builtins__': None}, {'x': value})
-                #for value, self.weights in zip(price_vec, self.weights)]
-       
-        
-        
-###########
 def gen_data(G, numItems):
     numNodes = nx.number_of_nodes(G)
-    
+
     for i in xrange(numNodes):
         ch = [lambda x : math.sqrt(x), lambda y : random.randint(0, numItems)*y, lambda z : 4 * math.sqrt(z)]
         utilVector = [random.choice(ch) for x in xrange(numItems)]
         #[random.choice([0,1]) for x in range(numItems)] #G.node[i]['utility'] = random.sample(xrange(num_items), num_items)
-        G.node[i]['util'] = utilVector 
-        G.node[i]['price_vec'] = [float(random.randint(1,10)) for x in range(numItems)] #[random.sample(xrange(1,10),numItems)]]
-        G.node[i]['endow'] = random.sample(xrange(1,10),numItems)
-        G.node[i]['earning'] = [x*y for x,y in zip(G.node[i]['price_vec'], G.node[i]['endow'])]
+        G.node[i]['util'] = utilVector
+        #G.node[i]['price_vec'] = [float(random.randint(1,10)) for x in range(numItems)] #[random.sample(xrange(1,10),numItems)]]
+        #G.node[i]['endow'] = random.sample(xrange(1,10),numItems)
+        G.node[i]['price_vec'] = np.array(map(int, raw_input().split(',')))
+        G.node[i]['endow'] = np.array(map(int, raw_input().split(',')))
+        G.node[i]['budget'] = sum([x*y for x,y in zip(G.node[i]['price_vec'], G.node[i]['endow'])])
         G.node[i]['expenditure'] = [0] * numItems
 
-###########
-def check_rationality(G, neighbor, bestNeighbour, i,  idx):
-    if G.node[neighbor]['endow'][idx]/float(G.node[i]['earning'][idx]) < G.node[bestNeighbour]['endow'][idx]/float(G.node[i]['earning'][idx]):
-        return bestNeighbour
-    else:
-        return neighbor
-        
 
-###########
-def find_best_price(G, i, idx, nList):
-    bestPrice = G.node[i]['price_vec'][idx]
-    bestNeighbour = i
-    
-    for neighbor in nx.all_neighbors(G, i):
-        print 'neighbor : %d | Price vector : %d' %(neighbor, G.node[neighbor]['price_vec'][idx])
-        
-        if G.node[neighbor]['price_vec'][idx] < bestPrice:
-            bestPrice = G.node[neighbor]['price_vec'][idx]
-            bestNeighbour = neighbor
-        
-        elif G.node[neighbor]['price_vec'][idx] == bestPrice:
-            bestNeighbour = check_rationality(G, neighbor, bestNeighbour, i, idx)
-            bestPrice = G.node[bestNeighbour]['price_vec'][idx]
+def is_better_price(best_prices, price_vec_n, item):
+    if best_prices[item] > price_vec_n[item]:
+        return True
+    return False
 
-    print 'best price of commodity %s is %d from trader %d' %(idx, bestPrice, bestNeighbour)
-    
-    return bestNeighbour, bestPrice
-
-############
-def market_status(G, numTraders, numItems):
-    totalPurchased = 0
-    totalSold = 0
+def checkClearing(G, best_prices, tracking):
+    bundle = []
     for i in G.nodes_iter():
-        totalSold += sum(G.node[i]['endow'])
+        print "Enter utility of each good for node {}: ".format(i)
+        utility_vec = np.array(map(int, raw_input().split(',')))
+        U = LinearUtility(utility_vec)
+        print best_prices[i]
+        print U.bestPurchase(np.array(best_prices[i]), G.node[i]['budget'])
+        #print bundle[i]
 
-    print 'total items sold : ', totalSold
-
+def market(G):
+    """Checks whether equilibrium exists for given network"""
+    tracking = {} # make dictionary of dictionary to keep track of item -> neighbour
+    best_prices = {}
     for i in G.nodes_iter():
         nList = G.neighbors(i)
-        print 'trader : ', i
-        #print 'utility ', G.node[i]['util']
-        print 'price vector ', G.node[i]['price_vec']
-        print 'endowment ', G.node[i]['endow']
-        print 'earning ', G.node[i]['earning']
-        print 'list of neighbours : %s' %nList
+        print 'Node: ', i
+        print 'Node {}\'s prices for goods: {}'.format(i,G.node[i]['price_vec'])
+        print 'Node {}\'s endowment: {}'.format(i,G.node[i]['endow'])
+        print 'Node {}\'s budget: {}'.format(i,G.node[i]['budget'])
+        print 'Node {}\'s neighbours: {}'.format(i,nList)
 
-        idx = 0
-        for item in G.node[i]['util']:
-            if item != 0:
-                #idx = G.node[i]['util'].index(item)
-                print 'index ', idx
-                bestN, bestP = find_best_price(G, i, idx, nList)
-                G.node[i]['expenditure'][idx] = bestP * G.node[bestN]['endow'][idx]
-                idx += 1
-            else:
-                idx += 1
-        print 'expenditure : ', G.node[i]['expenditure']
-        print 'total earning : %.2f | total expenditure : %.2f' %(sum(G.node[i]['earning']), sum(G.node[i]['expenditure']))
-        
-        print
+        best_prices[i] = list(G.node[i]['price_vec']) # default best prices belong to node i itself
+        tracking[i] = {}
+        for item in range(0, len(G.node[i]['endow'])):
+            tracking[i][item] = i
+            for nn in nList:
+                if is_better_price(best_prices[i], G.node[nn]['price_vec'], item): # checking if neighbour has lesser or equal price for given item
+                    best_prices[i][item] = G.node[nn]['price_vec'][item]
+                    tracking[i][item] = nn
+
+        print 'best prices: ', best_prices[i]
+        print 'tracking: ', tracking[i]
+        print '---'
+
+    checkClearing(G, best_prices, tracking)
 
 
-#############
 def main():
     numTraders = int(raw_input("Enter the number of traders : "))
     numItems = int(raw_input("Enter the number of items : "))
-    print
-    G = nx.fast_gnp_random_graph(numTraders, 0.6)
-    gen_data(G, numItems)
+    G = nx.fast_gnp_random_graph(numTraders, 0.5)
     nx.draw(G, with_labels = True)
     plt.show()
-    market_status(G, numTraders, numItems)
+    gen_data(G, numItems)
+    market(G)
 
-##############
 if __name__ == "__main__":
     main()
