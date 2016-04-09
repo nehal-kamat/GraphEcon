37ca1ec72bfc03a54f330f05c018775deea8df15
Nehal Kamat <nehal.kamat@colorado.edu>
2016-04-09 17:17:45 -0600

    deleted line10

--- v2/graph-econ-v2.py
+++ v2/graph-econ-v2.py
@@ -8,7 +8,6 @@ import numpy as np
 def main():
     G = nx.Graph()
     numNodes = int(raw_input("Enter the number of nodes to be included in the graph: "))
-    edgeList = np.array([x for x in xrange(numNodes)])
     print "Print 1 for YES and 0 for NO"
     for x in xrange(numNodes-1):
         for y in xrange(1,numNodes):
@@ -20,6 +19,7 @@ def main():
                 G.add_edge(x,y)
             else:
                 continue
+
     nx.draw(G, with_labels = True)
     plt.savefig("InitialGraph.png")
 
