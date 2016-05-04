2c86ab325a0d8598aa97288c1bc1a2e7c83b05d5
Nehal Kamat <nehal.kamat@colorado.edu>
2016-05-04 16:33:16 -0600

    using quadratic LP solver

--- v2/graph-econ-v2.py
+++ v2/graph-econ-v2.py
@@ -1,13 +1,25 @@
 import networkx as nx
 from random import random
 import matplotlib.pyplot as plt
-from itertools import product
+from itertools import product, permutations, combinations
 import math
 import numpy as np
+import numdifftools as nd
+
+
+class Utility:
+    def __init__(self, weights):
+        self.weights = weights
+
+    def get_weights(self):
+        return self.weights
 
 class LinearUtility:
     def __init__(self, weights):
         self.weights = weights
+
+    def get_weights(self):
+        return self.weights
 
     def bestPurchase(self, price_vec, budget):
         price_vec = np.array(price_vec)
@@ -49,126 +61,144 @@ class Market:
         self.G = G.copy()
         self.utility = utility
         self.endowment = endowment
+        self.ngoods = 2
+        self.nnodes = len(self.G)
 
-    def bestPrices(self, prices):
-        self.prices = prices
-        for n in self.G:
-            self.G.node[n]['prices'] = prices[n]
-        budget = {}
-        for x in range(self.G.number_of_nodes()):
-            budget[x] = sum(self.prices[x]*self.endowment[x])
-            self.G.node[x]['budget'] = budget[x]
+    def getOptPlans(self, price_vec):
+        self.prices = price_vec
+        prices = self.prices
+        nnodes = self.nnodes
+        ngoods = self.ngoods
+        endowment = self.endowment
+        utility = self.utility
 
-        """searching for neighbours having the best prices for goods"""
-        best_prices = {}
-        best_neighbours = {}
-        keys={}
-        for n in self.G:
-            best_prices[n] = {}
-            best_neighbours[n] = {}
-            keys[n] = []
-            for good in range(0, len(self.endowment[n])):
-                best_neighbours[n][good] = []
-                best_neighbours[n][good].append(n)
-                min_price = self.prices[n][good]
-                best_prices[n][good] = min_price
-                for neighbor in self.G.neighbors(n):
-                    #if price of good X of neighbor y == min->add to list (tie)
-                    if self.prices[neighbor][good] == min_price:
-                        best_neighbours[n][good].append(neighbor)
-                    #if price of good X of neighbor y < create new list
-                    elif self.prices[neighbor][good] < min_price:
-                        min_price = self.prices[neighbor][good]
-                        best_neighbours[n][good] = []
-                        best_neighbours[n][good].append(neighbor)
-                        best_prices[n][good] = min_price
+        mat = {}
+        price_mat = {}
+        util_mat = {}
 
-            for good in best_prices[n]:
-                keys[n].append(best_prices[n][good])
+        adj = np.array([[1,1,0],[1,1,1],[0,1,1]])
 
-            self.G.node[n]['best prices'] = [best_prices[n][good] for good in best_prices[n]]
-            self.G.node[n]['best neighbours'] = [best_neighbours[n][good] for good in best_neighbours[n]]
+        for i in range(nnodes):
+            mat[i] = np.zeros((nnodes, ngoods))
+            price_mat[i] = np.zeros((nnodes, ngoods))
+            util_mat[i] = np.zeros((nnodes, ngoods))
+            for j in range(nnodes):
+                for k in range(ngoods):
+                    if adj[i][j] == 1:
+                        mat[i][j][k] = endowment[j][k]
+                        price_mat[i][j][k] = price_vec[j][k]
+                        wght = utility[i].get_weights()
+                        util_mat[i][j][k] = wght[k]
 
-        # format of best_prices, best_neighbours : {node: {good : ...}}
-        print "budget: {}\nbest prices: {}\nbest neighbours: {}".format(budget, best_prices, best_neighbours)
+        # print mat
+        # print price_mat
+        # print util_mat
 
-        # return keys, budget
+        end_rav = [np.ravel(mat[i], order='F') for i in range(nnodes)]
+        end_rav = [item for sublist in end_rav for item in sublist]
+        self.end_rav = end_rav
 
-        best_purchase = {}
-        for n in self.G:
-            best_purchase[n] = {}
-            best_purchase[n] = self.utility[n].bestPurchase(keys[n], budget[n])
-        print "purchase candidates: ", best_purchase
+        prices_rav = [np.ravel(price_mat[i], order='F') for i in range(nnodes)]
+        prices_rav = [item for sublist in prices_rav for item in sublist]
+        self.prices_rav = prices_rav
 
-        # return best_purchase
+        util_rav = [np.ravel(util_mat[i], order='F') for i in range(nnodes)]
+        util_rav = [item for sublist in util_rav for item in sublist]
+        self.util_rav = util_rav
 
-        final_purchase = {}
-        for n in self.G:
-            final_purchase[n] = []
-            print type(best_purchase[n][0])
-            if type(best_purchase[n][0])!=tuple:
-                bn = zip(best_purchase[n], self.G.node[n]['best neighbours'])
-                bn = [list(x) for x in bn]
-                for item in bn:
-                    temp=[]
-                    for ii in item[1]:
-                        if item[0] > self.endowment[ii][bn.index(item)]:
-                            temp.append(self.endowment[ii][bn.index(item)])
-                            diff = item[0] - self.endowment[ii][bn.index(item)]
-                            # print diff
-                            item[0]=diff
-                            if ii==len(item[1])-1 and diff!=0:
-                                remaining = diff
-                        else:
-                            temp.append(item[0])
-                    final_purchase[n].append(temp)
+        def l(i,j,k):
+            avg = len(self.end_rav) / nnodes
+            out = []
+            last = 0.0
+            while last < len(self.end_rav):
+                out.append(self.end_rav[int(last):int(last+avg)])
+                last += avg
+            avg2 = len(out[i]) / ngoods
+            out2 = []
+            last2 = 0.0
+            while last2 < len(out[i]):
+                out2.append(out[i][int(last2):int(last2+avg2)])
+                last2 +=avg2
 
-            elif type(best_purchase[n][0])==tuple:
-                for tup in best_purchase[n]:
-                    bn = zip(tup, self.G.node[n]['best neighbours'])
-                    bn = [list(x) for x in bn]
-                    print bn
-                    remaining = [0]*len(bn)
-                    temp=[]
-                    for item in bn:
-                        for ii in item[1]:
-                            if item[0] > self.endowment[ii][bn.index(item)]:
-                                temp.append(self.endowment[ii][bn.index(item)])
-                                diff = item[0] - self.endowment[ii][bn.index(item)]
-                                remaining[bn.index(item)]=diff
-                                item[0]=diff
-                                # if ii==len(item[1])-1 and diff!=0:
-                                #     remaining[bn.index(item)]=diff
-                            else:
-                                temp.append(item[0])
-                    print temp
-                    print remaining
-                    if remaining==[0 for x in range(len(remaining))]:
-                        print "yes"
-                        final_purchase[n].append(temp)
-                        # print rem
+            return out2[k-1][j]
 
-        print "final purchase: {}".format(final_purchase)
+        func = [(endowment[i][k]**2 - 2*endowment[i][k]*sum([l(j,i,k) for j in range(nnodes)]) + (sum([l(j,i,k) for j in range(nnodes)]))**2) for i in range(nnodes) for k in range(ngoods)]
 
-    # def bestPurchase(self, keys, budget):
+        func = np.array(func)
+
+        # print l(1,2,2)
+
+        A = np.zeros((2*nnodes, len(self.end_rav)))
+        B = np.array([4.0, 4.0, 4.0, -2.0, -2.0, -2.0])
+        # print B.shape
+        B = [item for item in np.ravel(B)]
+
+        div = len(self.end_rav) / nnodes
+
+        start = 0
+        for i in range(0, nnodes):
+            A[i][start : start + div] = self.prices_rav[start : start + div]
+            start = start + div
+
+        start = 0
+        for i in range(nnodes, 2*nnodes):
+            A[i][start : start + div] = self.util_rav[start : start + div]
+            start = start + div
+
+
+        def hessian(x):
+            """
+            Calculate the hessian matrix with finite differences
+            Parameters:
+               - x : ndarray
+            Returns:
+               an array of shape (x.dim, x.ndim) + x.shape
+               where the array[i, j, ...] corresponds to the second derivative x_ij
+            """
+            x_grad = np.gradient(x)
+            hessian = np.empty((x.ndim, x.ndim) + x.shape, dtype=x.dtype)
+            for k, grad_k in enumerate(x_grad):
+                # iterate over dimensions
+                # apply gradient again to every component of the first derivative.
+                tmp_grad = np.gradient(grad_k)
+                for l, grad_kl in enumerate(tmp_grad):
+                    hessian[k, l, :, :] = grad_kl
+
+            return hessian
+
+        M = hessian(func)
+        print M
+
+        # print hessian(A)
+        # print A
+        # H = nd.Hessian('forward')
+        # print H(A)
 
 
 def main():
     G = nx.Graph()
     numNodes = 3 #int(raw_input("Enter the number of nodes to be included in the graph: "))
     G.add_edges_from([(0,1),(1,2)])
-    nx.draw(G, with_labels = True)
-    plt.show()
+    # nx.draw(G, with_labels = True)
+    # plt.show()
     #plt.savefig("InitialGraph.png")
 
     endowment = {0:np.array([1,2]), 1:np.array([1,1]), 2:np.array([2,1])}
     utility = {0:LinearUtility([1,0]), 1:LinearUtility([1,1]), 2:LinearUtility([0,1])}
+    # utility = {0:Utility([1,0]), 1:Utility([1,1]), 2:Utility([0,1])}
 
     mkt = Market(G, utility, endowment)
 
     prices = {0:np.array([2,1]), 1:np.array([2,2]), 2:np.array([1,2])}
-
-    mkt.bestPrices(prices)
+    budget = {}
+    for n in G:
+        budget[n] = sum(endowment[n]*prices[n])
+    bp = {}
+    for n in G:
+        bp[n] = utility[n].bestPurchase(prices[n], budget[n])
+    # print bp
+    # mkt.bestPrices(prices)
+    mkt.getOptPlans(prices)
     # best_prices, budget     = mkt.bestPrices(prices)
     # best_purchase           = mkt.bestPurchase(best_prices, budget)
 
